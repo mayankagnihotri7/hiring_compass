bdecf5f050f34ed1cf5a1dc09e9ee4dff2582715
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2025-09-26 18:04:26 +0530

    add job model and migration, add validations to job model

---
+++ app/models/job.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Job < ApplicationRecord
+  enum status: %w[open closed paused].index_by(&:itself)
+
+  validates :title, :status, presence: true
+  validates :min_salary, :max_salary, numericality: { greater_than: 0 }
+
+  validate :max_salary_not_less_than_min_salary
+
+  belongs_to :user
+
+  private
+
+    def max_salary_not_less_than_min_salary
+      return if min_salary.blank? || max_salary.blank?
+
+      if min_salary > max_salary
+        errors.add(:max_salary, "must be greater than or equal to min salary")
+      end
+    end
+end
